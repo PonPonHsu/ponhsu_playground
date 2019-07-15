e4e44a21d29479b5349399eb37782db50000f805
PonPonHsu <ponponhsu731@gmail.com>
2019-07-15 14:52:31 +0800

    0715

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '305321020334-t5l9g9fftsnrovkae2ncrgdcqiklua6m.apps.googleusercontent.com',
+    googleClientSecret: '5BerLnBhxAImHQItI4_rbsI2',
+    mongoURI: 'mongodb+srv://PONHSU:vm354lacan731@cluster0-ch1yh.mongodb.net/test?retryWrites=true&w=majority', 
+    cookieKey: 'kejnwekngdfskne',
+    stripePublishableKey: 'pk_test_VWJSki9RuUzhGJuiJn0xVIl100tuj4k1m3',
+    stripSecretKey: 'sk_test_HycDersa9OWhkkDwDEMWUBAZ00ZZbhrYr3'
+};
